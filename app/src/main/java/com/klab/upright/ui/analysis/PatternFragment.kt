7806d9c914c4df1c52fa81389f0d74267272f80e
kia3415 <54172475+kia3415@users.noreply.github.com>
2020-06-21 15:08:08 +0900

    add BarChart

--- app/src/main/java/com/klab/upright/ui/analysis/PatternFragment.kt
+++ app/src/main/java/com/klab/upright/ui/analysis/PatternFragment.kt
@@ -7,6 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.klab.upright.R
 import kotlinx.android.synthetic.main.fragment_pattern.*
 import java.util.*
@@ -69,6 +74,88 @@ class PatternFragment : Fragment() {
             var builder = DatePickerDialog(requireContext(),datePickerListener,endDate.get(Calendar.YEAR),endDate.get(Calendar.MONTH),endDate.get(Calendar.DATE))
             builder.show()
         }
+
+        initBarChart()
+    }
+
+    private fun initChart() {
+//        val xAxis = lineChart.xAxis
+//
+//        xAxis.apply {
+//            position = XAxis.XAxisPosition.BOTTOM
+//            textSize = 10f
+//            setDrawAxisLine(false)
+//            granularity = 1f
+//            axisMinimum = 2f
+//            isGranularityEnabled = true
+//        }
+//
+//        lineChart.apply {
+//            axisRight.isEnabled = false
+//            axisLeft.axisMaximum = 50f
+//            legend.apply {
+//                textSize = 15f
+//                verticalAlignment = Legend.LegendVerticalAlignment.TOP
+//                horizontalAlignment =Legend.LegendHorizontalAlignment.CENTER
+//                orientation = Legend.LegendOrientation.HORIZONTAL
+//                setDrawInside(false)
+//            }
+//        }
+//
+//        val values = arrayListOf<Entry>()
+//        //앉아있는시간
+//
+//        //서있는시간
+//
+////        val lineDataSet = LineDataSet
+//
+//        val lineData = LineData()
+//        lineChart.data = lineData
+    }
+
+    fun initBarChart(){
+        val entries = arrayListOf<BarEntry>()
+        entries.add(BarEntry(5.5f,0))
+        entries.add(BarEntry(3f,1))
+
+        val depenses = BarDataSet(entries,"시간")
+        depenses.axisDependency = YAxis.AxisDependency.LEFT
+
+        var labels = arrayListOf<String>()
+        labels.add("앉아있는 시간")
+        labels.add("서있는 시간")
+
+        var data = BarData(labels,depenses)
+        data.setValueTextSize(16f)
+
+
+        depenses.setColors(ColorTemplate.PASTEL_COLORS)
+
+        val legend = barChart.legend
+        legend.apply {
+            textSize = 15f
+            formSize = 0f
+            //formSize = 30f
+            mTextHeightMax = 10f
+
+
+        }
+
+        val xAxis = barChart.xAxis
+        xAxis.apply {
+            setDrawGridLines(false)
+            textSize = 16f
+
+        }
+
+        val rightAxis = barChart.axisRight
+        rightAxis.isEnabled = false
+
+        barChart.data = data
+        barChart.setDescription(null)
+        barChart.animateXY(2000,2000)
+        barChart.invalidate()
+
     }
 
     fun updateData(){
