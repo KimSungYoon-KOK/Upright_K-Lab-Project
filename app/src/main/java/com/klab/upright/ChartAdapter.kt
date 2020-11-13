174554b307e4686069f0db8102f1bf6902ef2f68
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2020-11-13 19:26:32 +0900

    chart

--- app/src/main/java/com/klab/upright/ChartAdapter.kt
+++ app/src/main/java/com/klab/upright/ChartAdapter.kt
@@ -1,6 +1,7 @@
 package com.klab.upright
 
 import android.content.Context
+import android.graphics.DashPathEffect
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,17 +9,23 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.PagerAdapter
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IFillFormatter
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.klab.upright.ui.analysis.Time
 import kotlinx.android.synthetic.main.fragment_analysis.*
 import kotlinx.android.synthetic.main.layout_chart.view.*
+import kotlinx.android.synthetic.main.layout_chart2.view.*
 import java.util.*
+import kotlin.collections.ArrayList
 
-class ChartAdapter(val context:Context) : PagerAdapter() {
+class ChartAdapter(val context:Context, val itemList: ArrayList<Time>) : PagerAdapter()
+{
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
         return view == `object`
     }
@@ -29,17 +36,93 @@ class ChartAdapter(val context:Context) : PagerAdapter() {
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         val inflater = LayoutInflater.from(container.context)
-        val view = inflater.inflate(R.layout.layout_chart,null)
-
+        val view: View
         when(position){
             0->{
-
+                view = inflater.inflate(R.layout.layout_chart2,null)
+                val chart = view.pieChart
             }
             1->{
+                val totalList = arrayListOf<Int>(2,5,0,1,4,2,5,0,1,4)
 
+                view = inflater.inflate(R.layout.layout_chart,null)
+                val lineChart = view.lineChart
+                lineChart.apply {
+                    setBackgroundColor(ContextCompat.getColor(context,R.color.white))
+
+                }
+
+                val xAxis = lineChart.xAxis
+                xAxis.position = XAxis.XAxisPosition.BOTTOM
+                xAxis.setDrawGridLines(false)
+                val yAxis = lineChart.axisLeft
+                yAxis.setDrawGridLines(false)
+                lineChart.axisRight.isEnabled=false
+                lineChart.axisLeft.isEnabled=false
+                lineChart.axisLeft.setDrawGridLines(false);
+                lineChart.axisRight.setDrawGridLines(false);
+
+                val values = arrayListOf<Entry>()
+
+                for(i in totalList.indices){
+                    values.add(Entry(i.toFloat(),totalList[i].toFloat(),ContextCompat.getDrawable(context, R.drawable.button_shadow_purple)))
+                }
+
+                val set1:LineDataSet
+                if (lineChart.data != null &&
+                    lineChart.data.dataSetCount > 0
+                ) {
+                    set1 = lineChart.data.getDataSetByIndex(0) as LineDataSet
+                    set1.values = values
+                    set1.notifyDataSetChanged()
+                    lineChart.data.notifyDataChanged()
+                    lineChart.notifyDataSetChanged()
+                }else {
+                    set1 = LineDataSet(values,"Dataset 1")
+
+                    set1.apply {
+                        setDrawIcons(false)
+
+                        // black line and point
+                        color = ContextCompat.getColor(context,R.color.colorPrimary)
+                        setCircleColor(ContextCompat.getColor(context,R.color.colorPrimary))
+
+
+                        lineWidth=2f
+                        circleRadius = 6f
+                        circleHoleRadius = 4f
+
+                        // customize legend entry
+                        formLineWidth = 1f
+                        formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+                        formSize = 15f
+
+                        setDrawCircleHole(true)
+
+                        valueTextSize = 10f
+
+                        cubicIntensity = 0.2f
+                        setMode(LineDataSet.Mode.CUBIC_BEZIER)
+
+                        setDrawFilled(true)
+                        fillColor = ContextCompat.getColor(context,R.color.purple_light_background)
+                        fillFormatter =
+                            IFillFormatter { dataSet, dataProvider -> lineChart.axisLeft.axisMinimum }
+                    }
+
+
+
+                    val dataSets = arrayListOf<ILineDataSet>()
+                    dataSets.add(set1)
+
+                    val data = LineData(dataSets)
+
+                    lineChart.data = data
+
+                }
             }
             else->{
-
+                view = inflater.inflate(R.layout.layout_chart,null)
             }
         }
         container.addView(view)
@@ -47,7 +130,7 @@ class ChartAdapter(val context:Context) : PagerAdapter() {
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        super.destroyItem(container, position, `object`)
+//        super.destroyItem(container, position, `object`)
     }
 
     private fun setChartData() {
--- app/src/main/java/com/klab/upright/ui/analysis/AnalysisFragment.kt
+++ app/src/main/java/com/klab/upright/ui/analysis/AnalysisFragment.kt
@@ -5,34 +5,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.components.YAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.IFillFormatter
-import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.klab.upright.ChartAdapter
 import com.klab.upright.R
 import com.klab.upright.ui.memo.MemoData
 import kotlinx.android.synthetic.main.fragment_analysis.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class AnalysisFragment : Fragment() {
 
-    lateinit var startDate: Calendar
-    lateinit var endDate: Calendar
-    val memoList = ArrayList<MemoData>()
 
-    var start:Int=0
-    var end:Int =0
+    lateinit var memoDB: DatabaseReference
+    lateinit var timeDB: DatabaseReference
+
+    private lateinit var startDate: Calendar
+    private lateinit var endDate: Calendar
+
+    val memoList = ArrayList<MemoData>()
+    val timeList = ArrayList<Time>()
+
+    private var start:Int=0
+    private var end:Int =0
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -48,7 +45,13 @@ class AnalysisFragment : Fragment() {
     }
 
     private fun initViewPager(){
-        viewPager.setAdapter(ChartAdapter(requireContext()))
+        val itemList = ArrayList<Time>()
+        for (item in timeList) {
+            if (item.date in start..end) {
+                itemList.add(item)
+            }
+        }
+        viewPager.adapter = ChartAdapter(requireContext(), itemList)
     }
 
     private fun init() {
@@ -70,7 +73,8 @@ class AnalysisFragment : Fragment() {
                     startDate.set(year,month,dayOfMonth)
                     startText.text = "${year}.${month+1}.${dayOfMonth}"
                     start = year*10000+(month+1)*100+dayOfMonth
-                    updateData()
+                    updateMemoData()
+                    initViewPager()
                 }
 
             val builder = DatePickerDialog(requireContext(),datePickerListener,startDate.get(Calendar.YEAR),startDate.get(Calendar.MONTH),startDate.get(Calendar.DATE))
@@ -84,151 +88,54 @@ class AnalysisFragment : Fragment() {
                     endDate.set(year,month,dayOfMonth)
                     endText.text = "${year}.${month+1}.${dayOfMonth}"
                     end = year*10000+(month+1)*100+dayOfMonth
-                    updateData()
+                    updateMemoData()
+                    initViewPager()
                 }
 
             val builder = DatePickerDialog(requireContext(),datePickerListener,endDate.get(Calendar.YEAR),endDate.get(Calendar.MONTH),endDate.get(Calendar.DATE))
             builder.show()
         }
 
-        //initBarChart()
-        initChart()
-
-        FirebaseDatabase.getInstance().getReference().child("memo").addValueEventListener(object:ValueEventListener{
+        memoDB = FirebaseDatabase.getInstance().reference.child("memo")
+        memoDB.addValueEventListener(object:ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 memoList.clear()
-
-                for(item in snapshot.children){
+                for(item in snapshot.children) {
                     memoList.add(item.getValue(MemoData::class.java)!!)
-
                 }
-
-                updateData()
-                //TODO("Not yet implemented")
+                updateMemoData()
             }
 
             override fun onCancelled(error: DatabaseError) {
-                //TODO("Not yet implemented")
+//                TODO("Not yet implemented")
+            }
+        })
+
+
+        timeDB = FirebaseDatabase.getInstance().reference.child("WearingTime")
+        timeDB.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                timeList.clear()
+                for (item in snapshot.children) {
+                    timeList.add(item.getValue(Time::class.java)!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+//                TODO("Not yet implemented")
             }
 
         })
-    }
-
-    private fun initChart() {
-
-        lineChart.apply{
-            setViewPortOffsets(0f,0f,0f,0f)
-            setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
-
-            description.isEnabled = false
-            setTouchEnabled(false)
-            isDragEnabled = false
-            setScaleEnabled(false)
-            setBorderColor(ContextCompat.getColor(requireContext(), R.color.white))
-            setDrawGridBackground(false)
-        }
-
-        val x = lineChart.xAxis
-        x.isEnabled = false
-
-        val y = lineChart.axisLeft
-        y.apply {
-            textColor =ContextCompat.getColor(requireContext(), R.color.black)
-            textSize = 16f
-            setLabelCount(5)
-            setDrawLabels(true)
-            setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
-            axisLineColor = ContextCompat.getColor(requireContext(), R.color.black)
-            setDrawGridLines(true)
-        }
-
-        lineChart.axisRight.isEnabled = false
-
-        setChartData()
-
-
 
     }
 
-    private fun setChartData() {
-        //set data
-        var values = arrayListOf<Entry>()
-
-        for (i in 0 until 10) {
-            val v =Random().nextInt(10).toFloat()+3f
-            values.add(Entry(i.toFloat(), v))
-        }
-
-        val set1: LineDataSet
-        // create a dataset and give it a type
-        set1 = LineDataSet(values, "Sitting Time")
-        set1.mode = LineDataSet.Mode.CUBIC_BEZIER
-        set1.cubicIntensity = 0.2f
-        set1.setDrawFilled(true)
-        set1.setDrawCircles(false)
-        set1.lineWidth = 1.8f
-        set1.circleRadius = 4f
-        set1.setCircleColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
-        set1.highLightColor = ContextCompat.getColor(requireContext(), R.color.colorPrimary)
-        set1.color =ContextCompat.getColor(requireContext(), R.color.colorPrimary)
-        set1.fillColor = ContextCompat.getColor(requireContext(), R.color.colorPrimary)
-        set1.fillAlpha = 100
-        set1.setDrawHorizontalHighlightIndicator(false)
-        set1.fillFormatter =
-            IFillFormatter { dataSet, dataProvider -> lineChart.getAxisLeft().getAxisMinimum() }
-
-
-        //set data
-        var values2 = arrayListOf<Entry>()
-
-        for (i in 0 until 10) {
-            val v =Random().nextInt(4).toFloat()
-            values2.add(Entry(i.toFloat(), v))
-        }
-
-        val set2: LineDataSet
-
-        // create a dataset and give it a type
-        set2 = LineDataSet(values2, "Standing Time")
-        set2.mode = LineDataSet.Mode.CUBIC_BEZIER
-        set2.cubicIntensity = 0.2f
-        set2.setDrawFilled(true)
-        set2.setDrawCircles(false)
-        set2.lineWidth = 1.8f
-        set2.circleRadius = 4f
-        set2.setCircleColor(ContextCompat.getColor(requireContext(), R.color.colorPrimaryDark))
-        set2.highLightColor = ContextCompat.getColor(requireContext(), R.color.colorPrimaryDark)
-        set2.color =ContextCompat.getColor(requireContext(), R.color.colorPrimaryDark)
-        set2.fillColor = ContextCompat.getColor(requireContext(), R.color.colorPrimaryDark)
-        set2.fillAlpha = 100
-        set2.setDrawHorizontalHighlightIndicator(false)
-        set2.fillFormatter =
-            IFillFormatter { dataSet, dataProvider -> lineChart.getAxisLeft().getAxisMinimum() }
-
-        // create a data object with the data sets
-
-        val dataList = arrayListOf<ILineDataSet>()
-        dataList.add(set1)
-        dataList.add(set2)
-
-        val data = LineData(dataList)
-
-        //data.setValueTypeface(tfLight)
-        data.setValueTextSize(12f)
-        data.setDrawValues(true)
-        data.setValueTextColor(ContextCompat.getColor(requireContext(), R.color.colorPrimaryDark))
-
-        // set data
-        lineChart.setData(data)
-    }
-
-    fun updateData(){
+    fun updateMemoData() {
         //update data
         var time=0
         var pain=0
         var n=0
-        for(memo in memoList){
-            if(memo.date>=start && memo.date<=end){
+        for(memo in memoList) {
+            if(memo.date in start..end) {
                 time += memo.duration
                 pain += memo.pain
                 n++
@@ -239,50 +146,4 @@ class AnalysisFragment : Fragment() {
 //        exercise_text.text = time.toString()+"m"
 
     }
-
-//    fun initBarChart(){
-//        val entries = arrayListOf<BarEntry>()
-//        entries.add(BarEntry(5.5f,0))
-//        entries.add(BarEntry(3f,1))
-//
-//        val depenses = BarDataSet(entries,"시간")
-//        depenses.axisDependency = YAxis.AxisDependency.LEFT
-//
-//        var labels = arrayListOf<String>()
-//        labels.add("앉아있는 시간")
-//        labels.add("서있는 시간")
-//
-//        var data = BarData(labels,depenses)
-//        data.setValueTextSize(16f)
-//
-//
-//        depenses.setColors(ColorTemplate.PASTEL_COLORS)
-//
-//        val legend = barChart.legend
-//        legend.apply {
-//            textSize = 15f
-//            formSize = 0f
-//            //formSize = 30f
-//            mTextHeightMax = 10f
-//
-//
-//        }
-//
-//        val xAxis = barChart.xAxis
-//        xAxis.apply {
-//            setDrawGridLines(false)
-//            textSize = 16f
-//
-//        }
-//
-//        val rightAxis = barChart.axisRight
-//        rightAxis.isEnabled = false
-//
-//        barChart.data = data
-//        barChart.setDescription(null)
-//        barChart.animateXY(1500,1500)
-//        barChart.invalidate()
-//
-//    }
-
 }
